be5a5e3775fe24886fd96d65e7db88e1da70d566
JuanMartinCatuzzi <catuzzijuanmartin@gmail.com>
2019-04-25 23:50:44 -0300

    Primer Commit

---
+++ home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <!-- ANDY -->
+    </header>
+    <main>
+      <div class="imagen">
+        <img src="img/bookish-02.jpg" alt="" class="cabeza">
+      </div>
+      <section class="productos">
+        <div class="fondo-azul">
+
+        </div>
+        <div class="categorias">
+          <div class="img primera">
+            <img src="img/bookish-03.jpg" alt="" class="articulo">
+          </div>
+          <div class="img segunda">
+            <img src="img/bookish-04.jpg" alt="" class="articulo">
+          </div>
+        </div>
+      </section>
+      <section class="interes">
+        <h3 class="titulo">ARTICULOS DE INTERES</h3>
+        <article class="articulos">
+          <h4>TITULO</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <button type="button" name="button"class="boton">Seguir leyendo</button>
+        </article>
+        <article class="articulos">
+          <h4>TITULO</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <button type="button" name="button" class="boton">Seguir leyendo</button>
+        </article>
+        <article class="articulos">
+          <h4>TITULO</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <button type="button" name="button" class="boton">Seguir leyendo</button>
+        </article>
+      </section>
+    </main>
+    <footer>
+      <!--ANDY-->
+    </footer>
+  </body>
+</html>
